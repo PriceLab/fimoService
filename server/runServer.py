8807ed67ee68f7604d4e494de223bc7c2a0cbae1
paul-shannon <paul.thurmond.shannon@gmail.com>
2016-06-20 13:45:27 -0700

    added newlines only

--- server/runServer.py
+++ server/runServer.py
@@ -10,5 +10,6 @@ fimoExecutable = sys.argv[2]  #  "/Users/paul/meme/bin/fimo"
 motifsFile = sys.argv[3]       # = "/Users/paul/s/work/priceLab/cory/footprintWorkflow/JASPAR_CORE_plus_seth.meme"
 
 server = FimoServer(hostname, port, fimoExecutable, motifsFile)
+
 while True:
     server.handleRequest()
