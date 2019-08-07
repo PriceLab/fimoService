8ba95cfc064ce87fb9380b8007d79a0b56555add
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-08-07 13:15:35 -0700

    arabidopsis

--- pfms/prepareMatrixFile.R
+++ pfms/prepareMatrixFile.R
@@ -1,7 +1,8 @@
 library(MotifDb)
-pfms <- query(MotifDb, c("sapiens"), c("FLI1", "KLF1"))
+pfms <- query(MotifDb, c("sapiens", "jaspar2018", "MA0481.1"), c("foxp1"))
+pfms <- query(MotifDb, c("athaliana", "jaspar2018"))
 length(pfms)
 
-curated.names <- c("Hsapiens-jaspar2018-FLI1-MA0475.1", "Hsapiens-HOCOMOCOv10-KLF1_HUMAN.H10MO.C")
-pfms <- MotifDb[curated.names]
-export(pfms, "human-klf1-fli1.meme", 'meme')
+# curated.names <- c("Hsapiens-jaspar2018-FLI1-MA0475.1", "Hsapiens-HOCOMOCOv10-KLF1_HUMAN.H10MO.C")
+# pfms <- MotifDb[curated.names]
+export(pfms, "athaliana--jaspar2018.meme", 'meme')
